42ad4fdfc433336d226fa92f1d25ad140c251555
Patrick <patrikkarolyi@hotmail.com>
2019-10-03 14:14:55 +0200

    animated during run
    in 3 stages : idle, walking, run

---
+++ vrShooter/Assets/Scripts/Enemy/EnemyAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAnimator : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator animator;
+    [SerializeField]
+    private float updateTime;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float speedPercentage = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent",speedPercentage,updateTime,Time.deltaTime);
+    }
+}
