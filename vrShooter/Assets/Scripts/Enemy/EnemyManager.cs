686cde95b4e726897794e854b1e8f5cbb8415a47
Patrick <patrikkarolyi@hotmail.com>
2019-11-20 12:57:26 +0100

    enemyManager: new enemy on death

---
+++ vrShooter/Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] 
+    private List<Transform> spawnPoints;
+    
+    [SerializeField] 
+    private GameObject enemyPrefab;
+
+
+    public void OnEnemyDiedEvent()
+    {
+        Instantiate(enemyPrefab, spawnPoints.First().position, Quaternion.identity);
+    } 
+    
+    
+}
