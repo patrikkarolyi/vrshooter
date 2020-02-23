2f47355564e171311c92b72b59557ebefa6779f7
Patrik Károlyi <patrikkarolyi@hotmail.com>
2020-02-23 18:32:15 +0100

    mapTrigger and MapManager

---
+++ vrShooter/Assets/MapManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+public class MapManager : MonoBehaviour
+{
+    public int activeMapNum = 3;
+    public GameObject[] maps;
+    private Random r = new Random();
+    private List<GameObject> activeMaps;
+
+    void Start()
+    {
+        activeMaps = new List<GameObject>();
+        loadNext(new Vector3(0, 0, 0));
+    }
+
+    public void loadNext(Vector3 pos)
+    {
+        int index = r.Next(0, maps.Length);
+        activeMaps.Add(Instantiate(maps[index], pos, Quaternion.identity));
+
+        if (activeMaps.Count > activeMapNum)
+        {
+            Destroy(activeMaps.First());
+            activeMaps.RemoveAt(0);
+        }
+    }
+}
---
+++ vrShooter/Assets/MapTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MapTrigger : MonoBehaviour
+{
+    private MapManager mm;
+    private Transform playerTransform;
+
+    void Awake()
+    {
+        mm = FindObjectOfType<MapManager>();
+        playerTransform = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    void FixedUpdate()
+    {
+        if (playerTransform.position.z > transform.position.z)
+        {
+            mm.loadNext(playerTransform.position + new Vector3(0, 0, 20));
+            enabled = false;
+        }
+    }
+}
